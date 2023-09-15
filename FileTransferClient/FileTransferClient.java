25b3c31c8deceb25f3ff4795042f63e175293953
Hermano Castro <hermanocastro18@gmail.com>
2023-09-15 08:50:11 -0300

    add comments in the codes

--- FileTransferClient/FileTransferClient.java
+++ FileTransferClient/FileTransferClient.java
@@ -3,29 +3,38 @@ import java.net.*;
 
 public class FileTransferClient {
   public static void main(String[] args) {
+    // Verifica se três argumentos (porta, endereço IP do servidor e nome do arquivo) foram fornecidos ao iniciar o cliente
     if (args.length != 3) {
       System.err.println("Error: try java FileTransferClient <port> <server_ip> <file.extension>");
       System.exit(1);
     }
 
-    final int SERVER_PORT = Integer.parseInt(args[0]); // Argumento que recebe porta usada no servidor
-    final String SERVER_IP = args[1]; // Argumento que recebe o endereço IP do servidor
+    // Obtém a porta do servidor a partir do primeiro argumento fornecido na linha de comando
+    final int SERVER_PORT = Integer.parseInt(args[0]); 
 
-    String fileName = args[2]; // Argumento que recebe o nome do arquivo
+    // Obtém o endereço IP do servidor a partir do segundo argumento fornecido na linha de comando
+    final String SERVER_IP = args[1]; 
 
-    try (Socket clientSocket = new Socket(SERVER_IP, SERVER_PORT); PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true); FileInputStream fileInputStream = new FileInputStream(fileName); BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream)) {
+    // Obtém o nome do arquivo a ser transferido a partir do terceiro argumento fornecido na linha de comando
+    String fileName = args[2]; 
+
+    try (Socket clientSocket = new Socket(SERVER_IP, SERVER_PORT);
+         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+         FileInputStream fileInputStream = new FileInputStream(fileName);
+         BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream)) {
 
       // Enviar o nome do arquivo ao servidor
       out.println(fileName);
 
       // Enviar os dados do arquivo ao servidor
-      final int filesize= 6022386;
+      final int filesize = 6022386; // Tamanho máximo do buffer para receber os dados do arquivo
       byte[] buffer = new byte[filesize];
       int bytesRead;
       while ((bytesRead = bufferedInputStream.read(buffer)) != -1) {
         clientSocket.getOutputStream().write(buffer, 0, bytesRead);
       }
 
+      // Exibe uma mensagem indicando que o arquivo foi enviado com sucesso
       System.out.println("File uploaded successfully: " + fileName);
     } catch (IOException e) {
       e.printStackTrace();
--- FileTransferServer/FileTransferServer.java
+++ FileTransferServer/FileTransferServer.java
@@ -3,51 +3,56 @@ import java.net.*;
 
 public class FileTransferServer {
   public static void main(String[] args) {
+    // Verifica se um argumento (número da porta) foi fornecido ao iniciar o servidor
     if (args.length != 1) {
       System.err.println("Error: try java FileTransServer <port>");
       System.exit(1);
     }
 
-    final int PORT = Integer.parseInt(args[0]); // Argumento que recebe a porta que o servidor irá utilizar
+    // Obtém o número da porta a partir do primeiro argumento fornecido na linha de comando
+    final int PORT = Integer.parseInt(args[0]);
 
     try (ServerSocket serverSocket = new ServerSocket(PORT)) {
-
+      // Exibe a porta em que o servidor está conectado
       System.out.println("Server connected on port: " + PORT);
       System.out.println("Waiting for client...");
 
       while (true) {
+        // Aguarda e aceita conexões de clientes
         Socket clientSocket = serverSocket.accept();
-        System.out.println("Connection established with" + clientSocket.getInetAddress());
+        System.out.println("Connection established with " + clientSocket.getInetAddress());
 
-        // Receber o nome do arquivo do cliente
+        // Recebe o nome do arquivo enviado pelo cliente
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         String fileName = in.readLine();
         System.out.println("Receiving file: " + fileName);
 
-        // Abrir o arquivo para escrita no destino
+        // Abre o arquivo para escrita no destino
         File file = new File(fileName);
         FileOutputStream fileOutputStream = new FileOutputStream(file);
         BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
 
-        // Iniciar a contagem de tempo
+        // Inicia a contagem de tempo para medir a taxa de transferência
         long startTime = System.nanoTime();
 
-        // Receber e gravar os dados do arquivo
-        final int filesize= 6022386;
+        // Define o tamanho máximo do buffer para receber os dados do arquivo
+        final int filesize = 6022386;
         byte[] buffer = new byte[filesize];
         int bytesRead;
+
+        // Recebe e escreve os dados do arquivo no destino
         while ((bytesRead = clientSocket.getInputStream().read(buffer)) != -1) {
           bufferedOutputStream.write(buffer, 0, bytesRead);
         }
 
-        // Calcular a taxa de transferência
+        // Calcula a taxa de transferência em Mbps
         long endTime = System.nanoTime();
         double transferTime = (endTime - startTime) / 1e9; // segundos
         long fileSize = file.length(); // bytes
         double transferRateMbps = (fileSize / (1024 * 1024)) / transferTime;
         System.out.printf("Transfer rate: %.2f Mbps%n", transferRateMbps);
 
-        // Fechar conexões e arquivos
+        // Fecha conexões e arquivos após a transferência
         bufferedOutputStream.close();
         fileOutputStream.close();
         in.close();
